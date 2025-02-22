0770bcbd4a858ec067d3082c19a09a7472393e1f
shailesj121 <shaileshbisht9876@gmail.com>
2025-02-22 10:43:57 +0530

    import service worker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging"
 import './App.css'
+import "/service-worker.js"
 
 function App() {
   // Notification.requestPermission().then((permission)=> {
