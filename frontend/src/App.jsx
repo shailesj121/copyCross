f45e3c2a703af276e2c3e0da24c3ec82868124a8
shailesj121 <shaileshbisht9876@gmail.com>
2025-02-22 10:33:55 +0530

    add service worker to the app.jsx file for notification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,37 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging"
 import './App.css'
 
 function App() {
-  Notification.requestPermission().then((permission)=> {
-    if(permission == "granted"){
-      new Notification('Hello')
-    }
-  })
+  // Notification.requestPermission().then((permission)=> {
+  //   if(permission == "granted"){
+  //     new Notification('Hello')
+  //   }
+  // })
+
+  self.addEventListener('install', function(event) {
+    console.log('Service Worker installing.');
+    // Perform installation steps, like pre-caching resources
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+  });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+        console.log('Service Worker Registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+        console.error('Service Worker registration failed:', error);
+    });
+}
+
+
 
   return (
     <>
